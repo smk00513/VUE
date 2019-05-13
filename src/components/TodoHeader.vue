1a06123febd55de6bfe1c9bf81d0aca8c8be4d87
smk00513 <smk00513@naver.com>
2019-05-13 10:13:09 +0900

    Todolist vue
    Header, Footer, Input fin

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1> 오늘의 할일 </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  }
+
+</script>
+
+<style scoped>
+  h1{
+    color : #D1B2FF;
+    font-weight : 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+
+</style>
